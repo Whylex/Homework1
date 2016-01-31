1f98ba564d3d343104bb2b32a68c90fe4ce850c3
Whylex <lenivkinsasha@mail.ru>
2016-01-31 21:38:18 +0700

    homework1
    none

---
+++ Model/Cat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        private string _name;
+        private int _health = 5;        
+        public CatColor Color { get; set; }
+
+        public Cat(CatColor color, string age)
+        {
+            Color = color;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+
+        public string Age { get; }
+
+
+        public void Feed()
+        {
+            ++_health;
+        }
+
+        public void Punish()
+        {
+            --_health;
+        }
+
+        public string CurrentColor
+        {
+            get 
+            {
+                return _health< 5 ? Color.SickColor : Color.HealthyColor;
+            }
+        }
+    }
+}
---
+++ View/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace View
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var color = new CatColor();
+            string age;
+            
+            Console.WriteLine("Вы купили кошку!");
+
+            Console.Write("Введите возраст кошки: ");
+            age = Console.ReadLine();
+
+            var cat = new Cat(color, age);
+
+            int i;
+            do
+            {
+                Console.WriteLine("\nТекущая информация о кошке: \nИмя: " + cat.Name + "\nВозраст: " + cat.Age + "\nТекущий цвет: " + cat.CurrentColor);
+
+                Console.Write("\n\nМеню: \n1) Дать кошке имя \n2) Изменить цвет здоровой кошки \n3) Изменить цвет больной кошки \n4) Наказать \n5) Покормить \n6) Выйти \n\nВаше решение: ");
+
+                i = int.Parse(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        Console.Write("Введите имя кошки: ");
+                        cat.Name = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Введите цвет здоровой кошки: ");
+                        color.HealthyColor = Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Write("Введите цвет больной кошки: ");
+                        color.SickColor = Console.ReadLine();
+                        break;
+                    case 4:
+                        cat.Punish();
+                        break;
+                    case 5:
+                        cat.Feed();
+                        break;
+                    case 6:
+                        break;
+                }
+            } while (i != 6);
+        }
+    }
+}
